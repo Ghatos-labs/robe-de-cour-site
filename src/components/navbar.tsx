dac20470603782c27429af6b6ffbe1914aa569c2
Ghatos1 <marwan.ghatos.akkari@gmail.com>
2024-09-02 17:00:38 +0200

    added more stuff about the navbar, still unfinished

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 
-// function openNavbar()
-// {
-//   let mobileNavbar = document.getElementById("navbar");
-//   mobileNavbar?.setAttribute("style", "transform: translateX(0%);");
-// }
-
-// function closeNavbar()
-// {
-//   let mobileNavbar = document.getElementById("navbar");
-//   mobileNavbar?.setAttribute("style", "transform: translateX(-100%);");
-// }
+var isNavbarDeployed = false;
+function toggleNavbar()
+{
+  let mobileNavbar = document.getElementById("mobile-navbar");
+  switch(isNavbarDeployed)
+  {
+    case true: mobileNavbar?.setAttribute("style", "transform: translateX(0%);");
+    isNavbarDeployed = false;
+    break;
+    case false: mobileNavbar?.setAttribute("style", "transform: translateX(100%);")
+    isNavbarDeployed = true;
+    break;
+  }
+}
 
 function NavButtons() {
   return(
     <>
-      <a className="home-btn">Accueil</a>
-      <a className="dress-btn">Robes</a>
-      <a className="accessories-btn">Accesoires</a>
-      <a className="cart-btn">Panier</a>        
+      <a className="home-btn navbar-btn">Accueil</a>
+      <a className="dress-btn navbar-btn">Robes</a>
+      <a className="accessories-btn navbar-btn">Accesoires</a>
+      <a className="cart-btn navbar-btn">Panier</a>        
     </>
   )
 }
@@ -38,10 +41,11 @@ function Navbar() {
   return (
     <>
       <div id="mobile-navbar">
-        <a className="mobile-menu-btn">X</a>
+        <a className="menu-toggle-btn" onClick={toggleNavbar}>X</a>
         <NavButtons/>
       </div>
       <div id="desktop-navbar">
+        <a className="menu-toggle-btn" onClick={toggleNavbar}>+</a>
         <NavButtons/>
       </div>
       {/* <div id="navbar">
