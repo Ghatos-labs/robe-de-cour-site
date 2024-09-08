bf6b0afe034c8594610b55ea63609fb8fcd2a449
m.akkari <m.akkari@student.isartdigital.com>
2024-09-09 01:44:40 +0200

    kinda working navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,55 +6,60 @@ function toggleNavbar()
   let mobileNavbar = document.getElementById("mobile-navbar");
   switch(isNavbarDeployed)
   {
-    case true: mobileNavbar?.setAttribute("style", "transform: translateX(0%);");
-    isNavbarDeployed = false;
-    break;
-    case false: mobileNavbar?.setAttribute("style", "transform: translateX(100%);")
+    case false: mobileNavbar?.setAttribute("style", "transform: translateX(0%);");
     isNavbarDeployed = true;
+    break;
+    case true: mobileNavbar?.setAttribute("style", "transform: translateX(-100%);")
+    isNavbarDeployed = false;
     break;
   }
 }
 
 function NavButtons() {
   return(
-    <>
+    <div>
       <a className="home-btn navbar-btn">Accueil</a>
       <a className="dress-btn navbar-btn">Robes</a>
       <a className="accessories-btn navbar-btn">Accesoires</a>
       <a className="cart-btn navbar-btn">Panier</a>        
-    </>
+    </div>
   )
 }
 
 function Navbar() {
-  // React.useEffect(() => {
-  //   function handleResize()
-  //   {
-  //     if (window.innerWidth > 720)
-  //     {
-  //       let mobileNavbar = document.getElementById("navbar");
-  //       mobileNavbar?.setAttribute("style", "transform: translateX(0%);");
-  //     }
-  //   }
-  //   window.addEventListener('resize', handleResize);
-  // })
+  React.useEffect(() => {
+    if (isNavbarDeployed == false)
+    {
+      let mobileNavbar = document.getElementById("mobile-navbar");
+      mobileNavbar?.setAttribute("style", "transform: translateX(-100%);");
+    }
+    function handleResize()
+    {
+      let mobileNavbar = document.getElementById("mobile-navbar");
+      let desktopMenuBtn = document.getElementById("desktop-menu-btn");
+      if (window.innerWidth > 720)
+      {
+        mobileNavbar?.setAttribute("style", "transform: translateX(-100%);");
+        desktopMenuBtn?.setAttribute("style", "visibility: hidden;");
+        isNavbarDeployed = false;
+      }
+      else
+      {
+        desktopMenuBtn?.setAttribute("style", "visibility: visible;");
+      }
+    }
+    window.addEventListener('resize', handleResize);
+  })
   return (
     <>
       <div id="mobile-navbar">
-        <a className="menu-toggle-btn" onClick={toggleNavbar}>X</a>
+        <a className="menu-toggle-btn" id="mobile-menu-btn" onClick={toggleNavbar}>X</a>
         <NavButtons/>
       </div>
       <div id="desktop-navbar">
-        <a className="menu-toggle-btn" onClick={toggleNavbar}>+</a>
+        <a className="menu-toggle-btn" id="desktop-menu-btn" onClick={toggleNavbar}>+</a>
         <NavButtons/>
       </div>
-      {/* <div id="navbar">
-        <a className="mobile-menu-btn" onClick={closeNavbar}>X</a>
-        <a className="home-btn">Accueil</a>
-        <a className="dress-btn">Robes</a>
-        <a className="accessories-btn">Accesoires</a>
-        <a className="cart-btn">Panier</a>        
-      </div>   */}
     </>
   )
 }
