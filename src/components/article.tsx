99f8c3e49bc1437cbe257439eeca17218f72cb3c
Ghatos1 <marwan.ghatos.akkari@gmail.com>
2024-09-23 17:00:11 +0200

    started working on article page

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -1,3 +1,20 @@
+import data from "./confections-data.json";
+
+// const generateOptions = () => {
+//     const elementList: JSX.Element[] = [];
+//     for(let i = 1; i <= Object.keys(data.confectionPage.robes).length; i++)
+//     {
+//       elementList.push
+//       (
+//         <Link to={"/confections/" + data.confectionPage.accesoires[i - 1].id} className="confection-btn">
+//           <img src="https://placehold.co/100x150" key={i}></img>
+//           <h3>{data.confectionPage.accesoires[i - 1].name}</h3>
+//         </Link>
+//       );
+//     }
+//     return(elementList);
+// }
+
 function Article() {
     return (
         <div className="content-container">
@@ -5,8 +22,9 @@ function Article() {
                 <h1>Article</h1>
                 <div>
                     <img src="https://placehold.co/150x200"></img>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    <a>Ajouter au panier</a>
+                    <p id="article-description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                    <a id="add-cart-btn">Ajouter au panier</a>
                 </div>
             </div>
         </div>
