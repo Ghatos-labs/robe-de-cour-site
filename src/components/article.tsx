d33e0bc7fcd4dd0f05db1a29610c75b2de3c932e
Ghatos1 <marwan.ghatos.akkari@gmail.com>
2024-10-07 16:56:57 +0200

    rearranged JSON file and optimized code

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -3,57 +3,43 @@ import data from "./confections-data.json";
 
 const getArticleID = () => {
     const {articleID} = useParams();
-    const list = data.confectionPage.robes;
+    const list = data.confectionPage;
     const id = list.findIndex(Object => Object.id === articleID);
-    if (data.confectionPage.robes[id] == undefined)
-    {
-        const accesList = data.confectionPage.accesoires;
-        const newId = accesList.findIndex(Object => Object.id === articleID);
-        return newId;
-    }
     return id;
 }
 
-const getMaterialList = () => {
-    const elemID = getArticleID();
-    const elementList: JSX.Element[] = [];
-    var materialPropAdress = data.confectionPage.robes[elemID].options.material;
-    for(let i = 1; i <= Object.keys(materialPropAdress).length; i++)
-    {
-        elementList.push
-        (
-            <option>{materialPropAdress[i - 1]}</option>
-        );
-    }
-    return(elementList);
-}
+// const getMaterialList = () => {
+//     const elemID = getArticleID();
+//     const elementList: JSX.Element[] = [];
+//     var materialPropAdress = data.confectionPage[elemID].options.material;
+//     for(let i = 1; i <= Object.keys(materialPropAdress).length; i++)
+//     {
+//         elementList.push
+//         (
+//             <option>{materialPropAdress[i - 1]}</option>
+//         );
+//     }
+//     return(elementList);
+// }
 
-const getDoublureList = () => {
-    const elemID = getArticleID();
-    const elementList: JSX.Element[] = [];
-    var doublurePropAdress = data.confectionPage.robes[elemID].options.doublure;
-    for(let i = 1; i <= Object.keys(doublurePropAdress).length; i++)
-    {
-        elementList.push
-        (
-        <option>{doublurePropAdress[i - 1]}</option>
-        );
-    }
-    return(elementList);
-}
+// const getDoublureList = () => {
+//     const elemID = getArticleID();
+//     const elementList: JSX.Element[] = [];
+//     var doublurePropAdress = data.confectionPage[elemID].options.doublure;
+//     for(let i = 1; i <= Object.keys(doublurePropAdress).length; i++)
+//     {
+//         elementList.push
+//         (
+//         <option>{doublurePropAdress[i - 1]}</option>
+//         );
+//     }
+//     return(elementList);
+// }
 
 function Article() {
     const elemID = getArticleID();
-    var elemAdress;
+    const elemAdress = data.confectionPage[elemID]
 
-    if (data.confectionPage.robes[elemID].id != "eleve" || "avocat" || "auditeur" || "magistrat")
-    {
-        elemAdress = data.confectionPage.robes[elemID];
-    }
-    else
-    {
-        elemAdress = data.confectionPage.accesoires[elemID];
-    }
     return (
         <div className="content-container">
             <div id="article-container">
@@ -65,11 +51,11 @@ function Article() {
                     <form id="article-form">
                         <label htmlFor="robe-color">Couleur</label>
                         <select className="global-btn-style">
-                            <>{getMaterialList()}</>
+                            {/* <>{getMaterialList()}</> */}
                         </select>
                         <label htmlFor="robe-color">Doublure</label>
                         <select className="global-btn-style">
-                            <>{getDoublureList()}</>
+                            {/* <>{getDoublureList()}</> */}
                         </select>
                         <input type="submit" value="Ajouter au panier" className="add-cart-btn global-btn-style"></input>
                     </form>
--- src/components/confections.tsx
+++ src/components/confections.tsx
@@ -1,36 +1,20 @@
 import { Link } from "react-router-dom";
 import data from "./confections-data.json";
 
-interface Props {
-  btnAmount: number;
-}
-
-const GenerateRobesBtn = () => {
+const GenerateConfectionBtn = (isElemRobe: boolean) => {
   const elementList: JSX.Element[] = [];
-  for(let i = 1; i <= Object.keys(data.confectionPage.robes).length; i++)
+  for(let i = 0; i < data.confectionPage.length; i++)
   {
-    elementList.push
-    (
-      <Link to={"/confections/" + data.confectionPage.robes[i - 1].id} className="confection-btn">
-        <img src="https://placehold.co/100x150" key={i}></img>
-        <h3>{data.confectionPage.robes[i - 1].name}</h3>
-      </Link>
-    );
-  }
-  return(elementList);
-}
-
-const GenerateAccessoriesBtn = () => {
-  const elementList: JSX.Element[] = [];
-  for(let i = 1; i <= Object.keys(data.confectionPage.accesoires).length; i++)
-  {
-    elementList.push
-    (
-      <Link to={"/confections/" + data.confectionPage.accesoires[i - 1].id} className="confection-btn">
-        <img src="https://placehold.co/100x150" key={i + 100}></img>
-        <h3>{data.confectionPage.accesoires[i - 1].name}</h3>
-      </Link>
-    );
+    if (data.confectionPage[i].isRobe == isElemRobe)
+    {
+      elementList.push
+      (
+        <Link to={"/confections/" + data.confectionPage[i].id} className="confection-btn">
+          <img src="https://placehold.co/100x150" key={i}></img>
+          <h3>{data.confectionPage[i].name}</h3>
+        </Link>
+      );      
+    }
   }
   return(elementList);
 }
@@ -40,11 +24,11 @@ function Confections() {
     <div className="content-container">
       <h2>Robes</h2>
       <div className="confections-container">
-        {GenerateRobesBtn()}
+        {GenerateConfectionBtn(true)}
       </div>
       <h2>Accesoires</h2>
       <div className="confections-container">
-        {GenerateAccessoriesBtn()}
+        {GenerateConfectionBtn(false)}
       </div>
     </div>
   )
