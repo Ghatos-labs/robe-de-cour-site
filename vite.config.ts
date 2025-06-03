bd4006c945a7e8e125d8065b106b5b44514a9a9b
Ghatos1 <marwan.ghatos.akkari@gmail.com>
2025-06-03 16:42:31 +0200

    edited the vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-//import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/robe-de-cour-site/',
   plugins: [react()],
 })
-
-// export default defineConfig({
-//   plugins: [react(), viteSingleFile()],
-//   build: {
-//     target: 'esnext',
-//     assetsInlineLimit: Infinity,
-//     cssCodeSplit: false,
-//     rollupOptions: {
-//       output: {
-//         manualChunks: undefined,
-//       },
-//     },
-//   },
-// });
