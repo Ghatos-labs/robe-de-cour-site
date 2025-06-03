d8fcbccf54adda4423c9d4e06a87310de31f3b1f
Ghatos1 <marwan.ghatos.akkari@gmail.com>
2025-06-03 16:37:50 +0200

    remove dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+//import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+
+// export default defineConfig({
+//   plugins: [react(), viteSingleFile()],
+//   build: {
+//     target: 'esnext',
+//     assetsInlineLimit: Infinity,
+//     cssCodeSplit: false,
+//     rollupOptions: {
+//       output: {
+//         manualChunks: undefined,
+//       },
+//     },
+//   },
+// });
